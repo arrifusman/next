3cba2d01832fc8766a4227582f1bfcdb5442fd50
Lord Arba <samuarrif@gmail.com>
2022-11-25 10:57:52 +0700

    Updates

--- _next/static/kDWpZQYi3b02zlMK_md_f/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-c6330651b4059cfd.js","static/chunks/pages/index-79cc4dfd405afad3.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
